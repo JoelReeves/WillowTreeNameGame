e19e09f6c82f4a4997c7ffd4fccf539e4dd11e96
joelreeves <jmrmb80@gmail.com>
2017-02-25 10:15:38 -0500

    Methods to show or dismiss progress dialogs

--- app/src/main/java/com/willowtreeapps/namegame/ui/NameGameBaseFragment.java
+++ app/src/main/java/com/willowtreeapps/namegame/ui/NameGameBaseFragment.java
@@ -1,12 +1,17 @@
 package com.willowtreeapps.namegame.ui;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import com.willowtreeapps.namegame.core.ApplicationComponent;
 import com.willowtreeapps.namegame.core.NameGameApplication;
+import com.willowtreeapps.namegame.util.DialogBuilder;
 
 public abstract class NameGameBaseFragment extends Fragment {
+
+    @Nullable private ProgressDialog progressDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,4 +21,16 @@ public abstract class NameGameBaseFragment extends Fragment {
     }
 
     protected abstract void inject(ApplicationComponent component);
+
+    protected void showProgressDialog() {
+        if (isAdded()) {
+            progressDialog = DialogBuilder.showProgressDialog(getActivity(), false);
+        }
+    }
+
+    protected void dismissProgressDialog() {
+        if (null != progressDialog && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
 }
